43eabc3fdbfc6d5f85b984d90a96568f155c2999
JonathanFL <j.lydemann@gmail.com>
2020-12-09 17:45:29 +0100

    Update HighscoreWebSocket.tsx

--- dualnback_web3/src/WebSocket/HighscoreWebSocket.tsx
+++ dualnback_web3/src/WebSocket/HighscoreWebSocket.tsx
@@ -3,7 +3,7 @@ class HighscoreWebSocket{
 
     start = async (callback:any) => {
         // Create WebSocket connection.
-        const socket = new WebSocket('ws://localhost:8080');
+        const socket = new WebSocket('ws://localhost:5000');
 
         // Connection opened
         socket.addEventListener('open', function (event) {
