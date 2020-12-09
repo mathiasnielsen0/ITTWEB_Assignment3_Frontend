9b7c71021b26922cd024dee6bec35b5319f05842
Tobias <tobias.monster@swaplanguage.com>
2020-12-09 13:36:50 +0100

    highscore

--- dualnback_web3/src/Pages/HighscoresPage.tsx
+++ dualnback_web3/src/Pages/HighscoresPage.tsx
@@ -1,4 +1,5 @@
-import React  from "react";
+import React from "react";
+import ScoreCard from "../Components/ScoreCard";
 
 
 interface IProps {
@@ -6,18 +7,64 @@ interface IProps {
 }
 
 interface IState {
-
+    scorecard:any[]
 }
 
 export default class HighscoresPage extends React.Component<IProps, IState> {
+    
+    constructor(props: IProps) {
+        super(props);
+        this.state = {
+            scorecard : []
+        }
+    }
 
-    render(){
+    componentDidMount(): void {
+        
+        for (let i = 0; i < 10; i++) {
+            this.sc[i] = <ScoreCard key={i} index={i+1} username={"Hans"} score={100}/>
+        }
+        this.setState({scorecard: this.sc});
+    }
+    sc: any = [];
+
+    addScore = () =>{
+        //this.sc.splice(2,0 , <ScoreCard key={this.sc.length+1} index={this.sc.length} username={"Hansel"} score={300} />);
+        this.sc[this.sc.length] = <ScoreCard key={this.sc.length+1} index={this.sc.length} username={"Hansel"} score={300} />
+        console.log(this.sc.length);
+        this.setState({scorecard: this.sc});
+    };
+    
+    render() {
         return (
-            <div id="HighscoresWrapper">
+            <div id="HomepageWrapper">
+                <p>
+                    THIS IS Highscore
+                </p>
                 <div>
-                    THIS IS HIGHSCORES
+                    <button onClick={this.addScore}>Click me</button>
+                </div>
+                <div className={"container"}>
+                    <div className={"row"}>
+                        <div className={"col-2"}>
+                            #
+                        </div>
+                        <div className={"col-4"}>
+                            <p>
+                                Username:
+                            </p>
+                        </div>
+                        <div className={"col-6"}>
+                            <p>
+                                Score:
+                            </p>
+                        </div>
+                    </div>
+                    {this.state.scorecard.map((card: any) => {
+                        return card
+                    })}
                 </div>
             </div>
-      )
+        )
     }
 }
