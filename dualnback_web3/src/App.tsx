794fad6a9c501980ce2028b78f2284efd0feea20
JonathanFL <j.lydemann@gmail.com>
2020-12-11 13:16:57 +0100

    Update App.tsx

--- dualnback_web3/src/App.tsx
+++ dualnback_web3/src/App.tsx
@@ -52,8 +52,8 @@ render(){
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
-                <Nav.Link href="/">Game</Nav.Link>
                 <Nav.Link href="/highscores">Highscores</Nav.Link>
+                <Nav.Link href="/">Game</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
